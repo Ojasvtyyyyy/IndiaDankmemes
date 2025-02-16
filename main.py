db7dcb5a70959bdf33e69530b597982d66c2b785
Ojasvtyyyyy <ojasvkushwah@gmail.com>
2025-02-17 03:03:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import asyncio
 from dotenv import load_dotenv
 import random
 from datetime import datetime, timedelta
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
 
 load_dotenv()
 
@@ -102,7 +110,13 @@ def main():
     dp.add_handler(CommandHandler('memeweek', meme_week))
     
     updater.start_polling()
-    updater.idle()
+    return updater
+
+def run_flask():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 
 if __name__ == '__main__':
-    main()
+    updater = main()
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+    updater.idle()
